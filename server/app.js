f0cff9e230da1d71f83f442ce8863a8331fd3bd4
mishra-ujjwal <ujjwalmishra9717@gmail.com>
2026-05-02 11:36:43 +0530

    deploying

--- server/app.js
+++ server/app.js
@@ -9,7 +9,11 @@ const taskRoutes    = require("./routes/task.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
